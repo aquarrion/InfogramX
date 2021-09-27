969a4099eb7b900a922a8acced2bcd1c908267a8
aquarrion <garyaguilar300@gmail.com>
2021-09-26 22:43:51 -0400

    intent

--- app/src/main/java/com/aquarrion/infogramx/LoginActivity.java
+++ app/src/main/java/com/aquarrion/infogramx/LoginActivity.java
@@ -2,6 +2,7 @@ package com.aquarrion.infogramx;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
@@ -14,6 +15,7 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     public void goCreateAccount(View view){
-        System.out.println("------");
+        Intent intent = new Intent(this, CreateAccountActivity.class);
+        startActivity(intent);
     }
 }
