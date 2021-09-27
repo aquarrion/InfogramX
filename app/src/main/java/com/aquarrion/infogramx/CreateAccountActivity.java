074ecd8e1c0acab09de8b98e04a1e5b2167a19a8
aquarrion <garyaguilar300@gmail.com>
2021-09-26 22:35:11 -0400

    Toolbar

---
+++ app/src/main/java/com/aquarrion/infogramx/CreateAccountActivity.java
@@ -0,0 +1,26 @@
+package com.aquarrion.infogramx;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+
+public class CreateAccountActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_account);
+
+        showToolBar(getString(R.string.title_crear_cuenta),true);
+    }
+
+    public void showToolBar(String titulo, boolean botonSubir){
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+        getSupportActionBar().setTitle(titulo);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(botonSubir);
+    }
+}
